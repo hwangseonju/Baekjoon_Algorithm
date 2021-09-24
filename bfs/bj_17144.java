9c4af75bcec9bd7e80b4ae3a7344795a755ce355
hwangseonju <tjswn0425@naver.com>
2021-09-24 16:38:00 +0900

    bj_17144 update

--- bfs/bj_17144.java
+++ bfs/bj_17144.java
@@ -1,5 +1,3 @@
-package com.ssafy.ws;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -16,7 +14,7 @@ public class bj_17144 {
 	static int T; // 시간
 	static int[][] dust; // 미세먼지
 	static List<Dust> list; // 미세먼지 위치
-	static int total; // 방에 있는 미세먼지 양
+	static int total; // 방에 있는 전체 미세먼지 양
 	static int top = -1; // 위쪽 공기청정기 - 행
 	static int bottom = -1; // 아래쪽 공기청저기 - 행
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -38,13 +36,12 @@ public class bj_17144 {
 				dust[r][c] = Integer.parseInt(st.nextToken());
 				if (dust[r][c] > 0) { // 미세먼지 위치 저장
 					list.add(new Dust(r, c, dust[r][c]));
-					dust[r][c] = 0; // 확산된 미세먼지로 값을 뿌려주기 위해 위치만 저장
 				}
 
 				if (dust[r][c] == -1) { // 공기청정기 위치 저장
 					if (top == -1) { // 먼저 입력된 줄이 위쪽
 						top = r;
-					} else { // 나중에 입력된 줄이 아래쪽
+					} else { 		// 나중에 입력된 줄이 아래쪽
 						bottom = r;
 					}
 				}
@@ -76,15 +73,14 @@ public class bj_17144 {
 		while (!queue.isEmpty()) {
 			int size = queue.size();
 			
-			if (T == 0) {
+			if (T == 0) {	// 주어진 시간이 모두 지나면 종료
 				return;
 			}
 			
-			// 다시 미세먼지 확산을 위해 0으로 값 초기화
+			// 확산된 미세먼지를 표시하기 위해 0으로 값 초기화(공기청정기는 제외)
 			for (int i = 0; i < R; i++) {
 				for (int j = 0; j < C; j++) {
 					if(dust[i][j]>0) {
-						
 						dust[i][j]=0;
 					}
 				}
@@ -131,18 +127,18 @@ public class bj_17144 {
 	// 공기청정기 가동
 	static void airClean() {
 
-		int d = 0; // 방향
-		int r = top; // 행
-		int c = 0; // 열
+		int d = 0;		// 방향
+		int r = top; 	// 행
+		int c = 0; 		// 열
 		
 		// 위쪽 - 반시계 방향 회전
 		while (d < 4) {
 			int nr = r + topMove[d][0];
 			int nc = c + topMove[d][1];
 			if (nr>=0 && nr<=top && nc>=0 && nc<C) {
-				if(dust[r][c]==-1) {
+				if(dust[r][c]==-1) {	// 미세먼지가 공기청정기로 들어감
 					dust[r][c]=-1;
-				}else if(dust[nr][nc] == -1){
+				}else if(dust[nr][nc] == -1){	// 공기청정기에서 바람이 나옴
 					dust[r][c]=0;
 				}else {
 					dust[r][c] = dust[nr][nc];
@@ -178,7 +174,7 @@ public class bj_17144 {
 	}
 
 	static class Dust {
-		int x, y, w;
+		int x, y, w;	// x = x좌표, y = y좌표, w = 미세먼지양
 
 		public Dust(int x, int y, int w) {
 			super();
